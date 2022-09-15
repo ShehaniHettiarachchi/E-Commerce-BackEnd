40c5894995a9e3d99d57cc2c928dd1c3bdbc2dea
Ashan K Nimsara <ashanknimsara@gmail.com>
2022-09-15 11:29:42 +0530

    retrieve and update/order

--- models/OrderModel.js
+++ models/OrderModel.js
@@ -43,7 +43,7 @@ const orderSchema = new Schema({
     ],
     customer: {
         type: String,
-                required: true,
+        required: true,
     },
 
     totalPrice: {
--- routes/OrderRoutes.js
+++ routes/OrderRoutes.js
@@ -3,7 +3,7 @@ const { Customer } = require("../models/CustomerModel");
 const { Order } = require("../models/OrderModel");
 
 //Localhost:8070/order/newOrder
-router.route("/newOrder").post((req,res)=>{
+router.route("/newOrder").post((req, res) => {
 
     const {
         shippingInfo,
@@ -24,13 +24,47 @@ router.route("/newOrder").post((req,res)=>{
         deliveredAt,
         createdAt,
     });
-    
-    newOrder.save().then(()=>{
+
+    newOrder.save().then(() => {
         res.json("Order Added Successfully")
     }).catch(err => {
         console.log(err);
     })
 
 })
+
+// retrive all orders
+
+router.route("/").get((req, res) => {
+    Order.find().then((Order) => {
+        res.json(Order)
+    }).catch((err) => {
+        console.log(err);
+    })
+
+})
+
+//update order status
+router.route("/updateStatus/:id").put(async (req, res) => {
+    const orderId = req.params.id;
+    const { orderStatus } = req.body;
+
+    const updateStatus = {
+        orderStatus,
+
+    }
+    const update = await Order.findByIdAndUpdate(orderId, updateStatus).then(() => {
+
+        res.status(200).send({ status: "Order updated Successfully" });
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).send({ status: "Error with updating data", Order: update });
+
+    })
+
+})
+
+
+
 
 module.exports = router;
