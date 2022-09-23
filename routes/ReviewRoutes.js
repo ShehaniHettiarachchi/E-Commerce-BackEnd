59d9b93d3ec33e0ff63150a9b10af293a510ba3d
Niruni Imesha <niruniimesha@gmail.com>
2022-09-23 18:46:21 +0530

    review crud

--- routes/ReviewRoutes.js
+++ routes/ReviewRoutes.js
@@ -31,10 +31,73 @@ http: router.post("/add", (req, res) => {
     })
 })
 
+//localhost:8070/review/update/id ---> update
+http: router.route("/update/:id").put(async(req, res) => {
+    let ID = req.params.id;
+
+    const { 
+        userId,
+        productId,
+        rating,
+        comment,
+        time,
+    } = req.body;
+
+    const updateReview = {
+        userId,
+        productId,
+        rating,
+        comment,
+        time,
+    };
+
+    await Review.findByIdAndUpdate(ID, updateReview)
+        .then(() => {
+            res.status(200).send({ status: "Review updated successfully..." });
+        })
+        .catch((err) => {
+            console.log(err);
+            res
+                .status(500)
+                .send({ status: "Error with updating Data...", error: err.message });
+        });
+});
+
+//localhost:8070/review/delete/id ---> delete
+//632d693cb524aeebaae1c4f5
+
+http: router.route("/delete/:id").delete(async(req, res) => {
+    let ID = req.params.id;
+
+    await Review.findByIdAndDelete(ID)
+
+    .then(() => {
+            res.status(200).send({ status: "Cart Item successfully Deleted" });
+        })
+        .catch((err) => {
+            console.log(err.message);
+            res
+                .status(500)
+                .send({ status: "Error with deleting item", error: err.message });
+        });
+});
+
+//localhost:8070/review/ ---> get all items
+htttp: router.route("/").get((req, res) => {
+    Review.find()
+        .then((cart) => {
+            res.json(cart);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
+
+
 //-----get reviews from one product-----
 
 router.route("/get/:id").get(async (req, res) => {
-    let productId = req.params.id;
+    let productId = req.params.productId;
 
     await Review.findById(productId)
       .then((review) => {
@@ -47,5 +110,7 @@ router.route("/get/:id").get(async (req, res) => {
           .send({ status: "Error with get user", error: err.message });
       });
   });
+
+
 
 module.exports = router;
